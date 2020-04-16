3b3f5e1a760658da65d41a035e871841fb03bf1e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-16 17:56:03 -0600

    The shopping list function works

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -15,7 +15,11 @@ function getRecipes(){
 }
 
 function getShoppingList(recipe_id){
-
+    return dB('recipe_ingredients as ri')
+      .where({recipe_id})
+      .join('recipes as r', 'ri.recipe_id', 'r.id')
+      .join('ingredients as i', 'ri.ingredient_id', 'i.id')
+      .select('r.name as Recipe_Name', 'i.name as Ingredient_Name', 'ri.quantity as Amount_Needed')
 }
 
 function getInstructions(recipe_id){
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -16,6 +16,17 @@ router.get('/', (req, res)=>{
         })
 })
 
+router.get('/:id/ingredients', (req, res)=>{
+    const id = req.params.id
+    Recipes.getShoppingList(id)
+        .then(list =>{
+            res.status(200).json(list)
+        })
+        .catch(err =>{
+            res.status(500).json({message: 'Failed to get Ingredients from server'})
+        })
+})
+
 
 
 
