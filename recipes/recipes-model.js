4562b8b12db1b9b9ae67d2159f0498bd041fefdb
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-16 17:42:22 -0600

    the inital Read Crud for all recipes works!

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -3,6 +3,22 @@ const dB = require('../data/db-config.js')
 
 module.exports = {
     //list of helper functions
+    getRecipes,
+    getShoppingList,
+    getInstructions
 }
 
 //helper functions go here
+
+function getRecipes(){
+    return dB('recipes')
+}
+
+function getShoppingList(recipe_id){
+
+}
+
+function getInstructions(recipe_id){
+
+}
+
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -6,6 +6,16 @@ const router = express.Router();
 
 //CRUD actions go here they will start with /api/recipe
 
+router.get('/', (req, res)=>{
+    Recipes.getRecipes()
+        .then(recs =>{
+            res.status(200).json(recs)
+        })
+        .catch(err =>{
+            res.status(500).json({message: 'Failed to get Recipies from server'})
+        })
+})
+
 
 
 
